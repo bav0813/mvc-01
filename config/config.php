8a9e81a0da2a7a386efa2e6c5263304f1a86502c
Shkodenko V. Taras <taras@shkodenko.com>
2018-04-16 20:26:12 +0300

    LC #5

--- config/config.php
+++ config/config.php
@@ -1,3 +1,16 @@
 <?php
 
 Config::set('site_name', 'Your Site Name');
+
+Config::set('languages', array('en', 'fr'));
+
+// Routes. Route name => method prefix
+Config::set('routes', array(
+    'default' => '',
+    'admin'   => 'admin_',
+));
+
+Config::set('default_route', 'default');
+Config::set('default_language', 'en');
+Config::set('default_controller', 'pages');
+Config::set('default_action', 'index');
--- lib/router.class.php
+++ lib/router.class.php
@@ -1,7 +1,7 @@
 <?php
 
-class Router
-{
+class Router{
+
     protected $uri;
 
     protected $controller;
@@ -9,7 +9,12 @@ class Router
     protected $action;
 
     protected $params;
-    protected $newUri;
+
+    protected $route;
+
+    protected $method_prefix;
+
+    protected $language;
 
     /**
      * @return mixed
@@ -43,8 +48,76 @@ class Router
         return $this->params;
     }
 
-    public function __construct($uri){
-        print_r('Ok! Router was called with uri: '.$uri);
+    /**
+     * @return mixed
+     */
+    public function getRoute()
+    {
+        return $this->route;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMethodPrefix()
+    {
+        return $this->method_prefix;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    public function __construct($uri)
+    {
+        $this->uri = urldecode(trim($uri, '/'));
+
+        // Get defaults
+        $routes = Config::get('routes');
+        $this->route = Config::get('default_route');
+        $this->method_prefix = isset($routes[$this->route]) ? $routes[$this->route] : '';
+        $this->language = Config::get('default_language');
+        $this->controller = Config::get('default_controller');
+        $this->action = Config::get('default_action');
+
+        $uri_parts = explode('?', $this->uri);
+
+        // Get path like /lng/controller/action/param1/param2/.../...
+        $path = $uri_parts[0];
+
+        $path_parts = explode('/', $path);
+
+        if ( count($path_parts) ){
+
+            // Get route or language at first element
+            if ( in_array(strtolower(current($path_parts)), array_keys($routes)) ){
+                $this->route = strtolower(current($path_parts));
+                $this->method_prefix = isset($routes[$this->route]) ? $routes[$this->route] : '';
+                array_shift($path_parts);
+            } elseif ( in_array(strtolower(current($path_parts)), Config::get('languages')) ){
+                $this->language = strtolower(current($path_parts));
+                array_shift($path_parts);
+            }
+            // Get controller - next element of array
+            if ( current($path_parts) ){
+                $this->controller = strtolower(current($path_parts));
+                array_shift($path_parts);
+            }
+            // Get action
+            if ( current($path_parts) ){
+                $this->action = strtolower(current($path_parts));
+                array_shift($path_parts);
+            }
+
+            // Get params - all the rest
+            $this->params = $path_parts;
+
+        }
+
     }
 
 }
